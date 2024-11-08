a5cdf858e9ec9985c93fe2597b6d2c4fe0338a7d
nyashamel <nyashamel2000@gmail.com>
2024-11-08 12:51:45 +0200

    Create node and linked list class

---
+++ linked_List.py
@@ -0,0 +1,13 @@
+class Node: 
+    def __init__(self,data):
+        self.data= data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        
