6fb1b51f1827eba4d899957986a3cf96612ca260
nyashamel <nyashamel2000@gmail.com>
2024-11-08 14:57:09 +0200

    Add insert_at_end and display methods

--- linked_List.py
+++ linked_List.py
@@ -9,5 +9,28 @@ class LinkedList:
 
     def insert_at_end(self, data):
         new_node = Node(data)
+        
+        if not self.head:
+            self.head = new_node
 
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def dispaly (self):
+        current = self.head
+
+        if not current:
+            print ('List is empty')
+            return
+
+        while current:
+            print(current.data, end= "->")
+            current = current.next
+            print('None')
+            
+
+        
         
